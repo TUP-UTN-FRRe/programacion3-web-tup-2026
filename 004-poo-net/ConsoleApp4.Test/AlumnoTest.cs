e8d855c94b5cfb7252fbda9c9626d1471173e3e6
Jose A. Fernandez <fernandezja@gmail.com>
2026-03-26 15:06:24 -0300

    [004] Ejemplo de Alumno

---
+++ 004-poo-net/ConsoleApp4.Test/AlumnoTest.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp4.Test
+{
+    public class AlumnoTest
+    {
+        [Fact]
+        public void TestSimple1()
+        {
+            var x = 1;
+            var y = 2;
+            var resultado = x + y;
+
+            Assert.Equal(3, resultado);
+
+        }
+
+
+        [Fact]
+        public void DebeCrearUnAlumnoConNombre()
+        {
+
+            var c1 = new Alumno();
+
+            c1.Nombre = "Programacion III";
+
+            Assert.Equal("Programacion III", c1.Nombre);
+
+        }
+    }
+}
---
+++ 004-poo-net/ConsoleApp4/ConsoleApp4/Alumno.cs
@@ -0,0 +1,31 @@
+﻿public class Alumno
+{
+    private string _nombre;
+
+    public Alumno()
+    {
+    }
+
+    public string Nombre
+    {
+        get { 
+            //accion
+            return _nombre; 
+        }
+        set {
+            //acccion 
+            _nombre = value;
+        }
+    }
+
+    public string Apellido { get; private set; }
+
+
+
+    //public void SetNombre(string nombre)
+    //{
+    //    _nombre = nombre;
+    //}
+
+
+}
