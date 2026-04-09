b241f5da4ff828402b6705dd8080db7b48146a76
Jose A. Fernandez <fernandezja@gmail.com>
2026-04-09 18:01:59 -0300

    [006] Javascript no-intrusivo

--- 006-html/scripts.js
+++ 006-html/scripts.js
@@ -1,18 +1,18 @@
 var titulo1 = document.getElementById("titulo1");
-        var img1 = document.getElementById("img1");
+var miimagen = document.getElementById("miimagen");
 
         //Codigo no intrusivo
-        img1.addEventListener("click", function() {
+        miimagen.addEventListener("click", function() {
             accion1();
         });
 
-         img1.addEventListener("mousemove", function() {
+         miimagen.addEventListener("mousemove", function() {
             accion1();
         });
 
 function accion1() {
     var dateTime = new Date();
-            titulo1.innerHTML = "Programacion III - "+dateTime.getHours() + ":" + dateTime.getMinutes() + ":" + dateTime.getSeconds() + ":" + dateTime.getMilliseconds();
+    titulo1.innerHTML = "Programacion III - "+dateTime.getHours() + ":" + dateTime.getMinutes() + ":" + dateTime.getSeconds() + ":" + dateTime.getMilliseconds();
 }
 
         
