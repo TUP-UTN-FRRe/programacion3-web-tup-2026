eb9ddb1ee3000f15210b30a3c71345a0ccbd3be6
Jose A. Fernandez <fernandezja@gmail.com>
2026-04-28 18:32:10 -0300

    [011] WebApi se agrego politica de CORS para permitir todo

--- 011-api-db/Starwars.Apps.WebApiApp/Program.cs
+++ 011-api-db/Starwars.Apps.WebApiApp/Program.cs
@@ -4,9 +4,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
