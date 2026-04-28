d20f29a111919e30201b12a4432816a5d4280664
Jose A. Fernandez <fernandezja@gmail.com>
2026-04-28 16:32:26 -0300

    [010] SqlConnection, SqlCommand, SqlDataReader

--- 010-ado-simple/SimpleSqlServerConsoleApp/Program.cs
+++ 010-ado-simple/SimpleSqlServerConsoleApp/Program.cs
@@ -8,7 +8,32 @@ var connectionString = "Server=localhost;Database=StarwarsGalaxy;Integrated Secu
 //1 Conexion 
 var conn = new SqlConnection(connectionString);
 
+//2 Comando
+var cmd = new SqlCommand();
+cmd.CommandText = "SELECT JediId, Nombre FROM dbo.Jedi";
+cmd.Connection = conn;
+cmd.CommandType = System.Data.CommandType.Text;
+
+
+//Ejecutar el comando 
 
 conn.Open();
 
+var reader = cmd.ExecuteReader();
+
 Console.WriteLine("OK: Connection established");
+
+while (reader.Read())
+{
+    //var nombre1 = reader[1].ToString();
+    //var nombre2 = reader.GetString(1);
+    var nombre = reader.GetString(reader.GetOrdinal("Nombre"));
+
+    Console.WriteLine($"Jedi: {nombre}");
+}
+
+
+
+
+
+conn.Close();
