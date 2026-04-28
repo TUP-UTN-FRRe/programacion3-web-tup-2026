00000907a9bdbdd0277b4ad7cf4a1a744f9f3a3d
Jose A. Fernandez <fernandezja@gmail.com>
2026-04-28 18:53:45 -0300

    [001] Web simple HTML y JS con "cargando"

--- 011-api-db/Starwars.Apps.WebHtmlSimpleApp/app.js
+++ 011-api-db/Starwars.Apps.WebHtmlSimpleApp/app.js
@@ -1,12 +1,24 @@
 const API_URL = 'https://localhost:7213/api/jedi';
 
-const btnCargar = document.getElementById('btn-cargar');
+const btnCargar  = document.getElementById('btn-cargar');
 const listaJedis = document.getElementById('lista-jedis');
-const errorMsg = document.getElementById('error-msg');
+const errorMsg   = document.getElementById('error-msg');
+const loader     = document.getElementById('loader');
+
+function mostrarLoader() {
+    loader.classList.remove('oculto');
+    btnCargar.disabled = true;
+}
+
+function ocultarLoader() {
+    loader.classList.add('oculto');
+    btnCargar.disabled = false;
+}
 
 btnCargar.addEventListener('click', function () {
     listaJedis.innerHTML = '';
     errorMsg.textContent = '';
+    mostrarLoader();
 
     fetch(API_URL)
         .then(function (response) {
@@ -33,5 +45,8 @@ btnCargar.addEventListener('click', function () {
         })
         .catch(function (error) {
             errorMsg.textContent = error.message;
+        })
+        .finally(function () {
+            ocultarLoader();
         });
 });
