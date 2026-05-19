4529da1ab3f7018229f38dfe9307cf3900760f33
Jose A. Fernandez <fernandezja@gmail.com>
2026-05-19 18:04:03 -0300

    [014] Partidos desde la capa de datos. Renderizado en Blazor component

---
+++ 014-demo/backend/TUP.Mundial.Datos/PartidoRepository.cs
@@ -0,0 +1,26 @@
+﻿using TUP.Mundial.Entidades;
+
+namespace TUP.Mundial.Datos
+{
+    public class PartidoRepository
+    {
+        public PartidoRepository() { }
+
+        public List<Partido> ObtenerListado()
+        {
+            var partidos = new List<Partido>();
+ 
+            for (int i = 1; i < 100; i++)
+            {
+                partidos.Add(new Partido()
+                {
+                    Local = new Equipo() { Nombre = $"Equipo Local {i}" },
+                    Visitante = new Equipo() { Nombre = $"Equipo Visitante {i}" }
+                });
+
+            }
+
+            return partidos;
+        }
+    }
+}
---
+++ 014-demo/backend/TUP.Mundial.Negocio/PartidoNegocio.cs
@@ -0,0 +1,19 @@
+﻿using TUP.Mundial.Datos;
+using TUP.Mundial.Entidades;
+
+namespace TUP.Mundial.Negocio
+{
+    public class PartidoNegocio
+    {
+
+        public PartidoNegocio() { 
+        
+        }
+
+        public List<Partido> ObtenerListado()
+        {
+            var partidoRepository = new PartidoRepository();
+            return partidoRepository.ObtenerListado();
+        }
+    }
+}
